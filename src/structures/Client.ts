6b3f422aa1ad02ad88939a414d7532080201276e
Lugent <35547583+Lugent@users.noreply.github.com>
2022-07-24 09:38:38 -0400

    Add database handler Update variables

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -1,6 +1,8 @@
 import { ApplicationCommandDataResolvable, Client, Collection, GatewayIntentBits } from "discord.js";
 import { readdirSync } from "fs";
 import { CommandStructure } from "../typings/Command";
+import initializeDatabase from "../functions/databaseSetup";
+import { AuraDatabases } from "../typings/Database";
 
 const intents: GatewayIntentBits[] = [
     GatewayIntentBits.DirectMessageReactions,
@@ -26,22 +28,23 @@ export class AuraClient extends Client {
         super({intents: intents});
     }
     public commands: Collection<string, CommandStructure> = new Collection();
-    
+    public database: AuraDatabases = initializeDatabase();
+
     public start(): void {
         this.registerModules();
         this.login(process.env.discordToken);
     }
 
     public async registerModules() {
-        let root_dir: string[] = await readdirSync(process.cwd() + "/src/commands").filter(dir => !dir.includes("."));
-        if (!root_dir.length) { return; }
+        let rootDir: string[] = await readdirSync(process.cwd() + "/src/commands").filter(dir => !dir.includes("."));
+        if (!rootDir.length) { return; }
 
-        for (let dir of root_dir) {
-            let dir_path: string = process.cwd() + "/src/commands/" + dir;
-            let files_dir: string[] = await readdirSync(dir_path);
-            for (let file of files_dir) {
-                let file_path = dir_path + "/" + file;
-                let command: CommandStructure = (await import(file_path))?.default;
+        for (let dir of rootDir) {
+            let dirPath: string = process.cwd() + "/src/commands/" + dir;
+            let filesDir: string[] = await readdirSync(dirPath);
+            for (let file of filesDir) {
+                let filePath = dirPath + "/" + file;
+                let command: CommandStructure = (await import(filePath))?.default;
                 this.commands.set(command.id, command);
             }
         }
