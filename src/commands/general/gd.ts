d62782ab4ba58f203ddb0d29780e64ffbe34f071
unknown <lucariopokemonaura@gmail.com>
2022-08-02 22:32:40 -0400

    GD Level Search

--- src/commands/general/gd.ts
+++ src/commands/general/gd.ts
@@ -1,3 +1,5 @@
+import request from "request"; // Because is the only working to talk with robtop's servers
+import { ConvertToGJSearch } from "../../structures/GD";
 import { CommandStructure } from "../../typings/Command";
 
 let command: CommandStructure = {
@@ -10,7 +12,24 @@ let command: CommandStructure = {
             },
 
             async execute({client, interaction}) {
-                
+                let endpoint_url = "http://www.boomlings.com/database/";
+                let searchData = {gameVersion: "21", binaryVersion: "35", secret: "Wmfd2893gb7", type: 0, page: "0", count: 10};
+                request.post(endpoint_url + "getGJLevels21.php", {form: searchData, headers: {}}, function(error, response, body) {
+                    if (error)
+                    {
+                        console.log(error);
+                    }
+                    else {
+                        let level = ConvertToGJSearch(body);
+                        for (let i = 0; i < level.levels.length; i++)
+                        {
+                            console.log(level.levels[i].name);
+                            console.log(level.levels[i].author);
+                            console.log(level.levels[i].song);
+                            console.log("");
+                        }
+                    }
+                });
             }
         }
     ]
